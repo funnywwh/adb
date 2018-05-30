6cdc15d4daca3d4a859ac33147cdca215ab788da
wwenhui <wwenhui@weforpay.com>
2018-05-30 10:15:46 +0800

    1.support in windows 2.fix install error,3.fix Start,4.fix Forward

--- adb.go
+++ adb.go
@@ -1,13 +1,25 @@
 package adb
 
 import (
+	"runtime"
+
 	"github.com/progrium/go-shell"
 )
 
 var (
-	Adb_Shell   = "shell_hx"
-	Adb_Install = "install_hx"
+	Adb_Shell   = "shell"
+	Adb_Install = "install"
+	Adb_Push    = "push"
 )
+
+func init() {
+	switch runtime.GOOS {
+	case "linux", "android":
+	case "windows":
+		shell.Shell = []string{"cmd.exe", "/c"}
+	}
+	shell.Panic = false
+}
 
 type Adb struct {
 }
@@ -28,7 +40,9 @@ func (this *Adb) Install(apk string, args ...interface{}) (err error) {
 	_args = append(_args, args...)
 	_args = append(_args, apk)
 	p := shell.Run(_args...)
-	err = p.Error()
+	if p.ExitStatus != 0 {
+		err = p.Error()
+	}
 	return
 }
 
@@ -43,8 +57,24 @@ func (this *Adb) Broadcast(action string, args ...interface{}) (err error) {
 
 func (this *Adb) Start(activity string, args ...interface{}) (err error) {
 	var _args []interface{}
-	_args = append(_args, "adb", "am", "start", activity)
+	_args = append(_args, "adb", Adb_Shell, "am", "start", activity)
 	_args = append(_args, args...)
+	p := shell.Run(_args...)
+	err = p.Error()
+	return
+}
+func (this *Adb) Forward(args ...interface{}) (err error) {
+	var _args []interface{}
+	_args = append(_args, "adb", "forward")
+	_args = append(_args, args...)
+	p := shell.Run(_args...)
+	err = p.Error()
+	return
+}
+
+func (this *Adb) Push(localPath, remotePath string) (err error) {
+	var _args []interface{}
+	_args = append(_args, "adb", Adb_Push, localPath, remotePath)
 	p := shell.Run(_args...)
 	err = p.Error()
 	return
