96ae509fe108dd6da479bdf5d8f92b3445e0b966
wwenhui <wwenhui@weforpay.com>
2018-06-01 14:48:00 +0800

    1.使用go-shell时，apk在带空格的路径下安装失败问题

--- adb.go
+++ adb.go
@@ -1,9 +1,12 @@
 package adb
 
 import (
+	"bytes"
+	"fmt"
+	"os/exec"
 	"runtime"
 
-	"github.com/progrium/go-shell"
+	"github.com/funnywwh/go-shell"
 )
 
 var (
@@ -22,60 +25,87 @@ func init() {
 }
 
 type Adb struct {
+	AdbName string
 }
 
-func (this *Adb) Shell(cmd string, args ...interface{}) (stdout string, err error) {
-	var _args []interface{}
-	_args = append(_args, "adb", Adb_Shell)
+type Shell struct {
+	ExitStatus uint32
+	Stdout     *bytes.Buffer
+	Stderr     *bytes.Buffer
+	cmd        *exec.Cmd
+	err        error
+}
+
+func Run(cmd string, args ...string) *Shell {
+	sh := new(Shell)
+	sh.Stderr = bytes.NewBuffer(nil)
+	sh.Stdout = bytes.NewBuffer(nil)
+	sh.cmd = exec.Command(cmd, args...)
+	sh.cmd.Stderr = sh.Stderr
+	sh.cmd.Stdout = sh.Stdout
+	err := sh.cmd.Run()
+	if err != nil {
+		sh.err = err
+	}
+	fmt.Printf("%s err:%v\n", sh.cmd.Args, err)
+	return sh
+}
+func (this *Shell) Error() (err error) {
+	err = this.err
+	return
+}
+func (this *Adb) Shell(cmd string, args ...string) (stdout string, err error) {
+	var _args []string
+	_args = append(_args, Adb_Shell, cmd)
 	_args = append(_args, args...)
-	p := shell.Run(_args...)
+	p := Run(this.AdbName, _args...)
 	err = p.Error()
 	stdout = p.Stdout.String()
 	return
 }
 
-func (this *Adb) Install(apk string, args ...interface{}) (err error) {
-	var _args []interface{}
-	_args = append(_args, "adb", Adb_Install)
+func (this *Adb) Install(apk string, args ...string) (err error) {
+	var _args []string
+	_args = append(_args, Adb_Install)
 	_args = append(_args, args...)
 	_args = append(_args, apk)
-	p := shell.Run(_args...)
-	if p.ExitStatus != 0 {
+	p := Run(this.AdbName, _args...)
+	err = p.Error()
+	return
+}
+
+func (this *Adb) Broadcast(action string, args ...string) (err error) {
+	var _args []string
+	_args = append(_args, Adb_Shell, "am", "broadcast", "-a", action)
+	_args = append(_args, args...)
+	p := Run(this.AdbName, _args...)
+	err = p.Error()
+	return
+}
+
+func (this *Adb) Start(activity string, args ...string) (err error) {
+	var _args []string
+	_args = append(_args, Adb_Shell, "am", "start", activity)
+	_args = append(_args, args...)
+	p := Run(this.AdbName, _args...)
+	if p.Stderr.Len() > 0 {
 		err = p.Error()
 	}
 	return
 }
-
-func (this *Adb) Broadcast(action string, args ...interface{}) (err error) {
-	var _args []interface{}
-	_args = append(_args, "adb", "am", "broadcast")
+func (this *Adb) Forward(args ...string) (err error) {
+	var _args []string
+	_args = append(_args, "forward")
 	_args = append(_args, args...)
-	p := shell.Run(_args...)
-	err = p.Error()
-	return
-}
-
-func (this *Adb) Start(activity string, args ...interface{}) (err error) {
-	var _args []interface{}
-	_args = append(_args, "adb", Adb_Shell, "am", "start", activity)
-	_args = append(_args, args...)
-	p := shell.Run(_args...)
-	err = p.Error()
-	return
-}
-func (this *Adb) Forward(args ...interface{}) (err error) {
-	var _args []interface{}
-	_args = append(_args, "adb", "forward")
-	_args = append(_args, args...)
-	p := shell.Run(_args...)
+	p := Run(this.AdbName, _args...)
 	err = p.Error()
 	return
 }
 
 func (this *Adb) Push(localPath, remotePath string) (err error) {
-	var _args []interface{}
-	_args = append(_args, "adb", Adb_Push, localPath, remotePath)
-	p := shell.Run(_args...)
+	var _args []string
+	_args = append(_args, Adb_Push, localPath, remotePath)
+	p := Run(this.AdbName, _args...)
 	err = p.Error()
 	return
 }
